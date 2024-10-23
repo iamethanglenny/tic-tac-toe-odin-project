eec00aa7fb3da5c1545595cedfb4eb4df183228d
iamethanglenny <ethanglenny@gmail.com>
2024-10-23 09:02:08 +0200

    feat: Add player factory, and two players, 1 and 2, with their respective symbols.

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const gameboardFactory = () => {
     return {
         board: ["", "", "", "", "", "", "", "", ""], // 3x3 grid for tic-tac-toe
-        displayBoard: function() {
+        displayBoard: function () {
             return this.board;
         },
         setMove: function (index, symbol) {
             if (this.board === "") {
                 this.board[index] = symbol;
             }
+        },
+        resetBoard: function () {
+            return this.board;
         }
     };
 };
+
+const gameControllerFactory = () => {
+
+}
+
+const playerFactory = (name, symbol) => {
+    return {
+        name: name,
+        symbol: symbol
+    };
+};
+
+const player1 = playerFactory("Player 1", "X");
+const player2 = playerFactory("Player2", "O");
