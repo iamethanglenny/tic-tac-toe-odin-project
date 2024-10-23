1bd93541d48eb479a486522731738b55c14c1e82
iamethanglenny <ethanglenny@gmail.com>
2024-10-23 10:43:11 +0200

    feat: Add function to identify when a player has won a round, function to end the round and game, to declare a winner, and to update each player's respective scores.

--- script.js
+++ script.js
@@ -12,40 +12,120 @@ const gameboardFactory = () => {
     };
 };
 
-const gameboard = gameboardFactory();
-gameboard.setMove(0, "X");
-gameboard.displayBoard();
+// console log to check above code.
+    const gameboard = gameboardFactory();
+    gameboard.setMove(0, "X");
+    gameboard.setMove(1, "O");
+    gameboard.displayBoard();
 
 const playerFactory = (name, symbol) => {
+    let score = 0;
     return {
         name: name,
         symbol: symbol,
+        score: score,
     };
 };
 
-const player1 = playerFactory("Player 1", "X");
-const player2 = playerFactory("Player2", "O");
-
-console.log(player1);
+// console log to check above code.
+    const player1 = playerFactory("Player 1", "X");
+    const player2 = playerFactory("Player 2", "O");
+    console.log(player1);
+    console.log(player2);
 
 const gameControllerFactory = (player1, player2, gameboard) => {
     let currentPlayer = player1;
+    let roundsPlayed = 0;
 
     return {
         switchTurn: function () {
             currentPlayer = currentPlayer === player1 ? player2 : player1;
         },
+
         displayCurrentPlayer: function () {
             console.log(`It's ${currentPlayer.name}'s turn to move.`);
         },
+
         playMove: function (index) {
             gameboard.setMove(index, currentPlayer.symbol);
+
+            if (this.checkWinner(currentPlayer)) {
+                this.setScore(currentPlayer);
+                this.endRound();
+            } else if (this.isBoardFull(gameboard.board)) {
+                console.log("It's a draw!");
+                this.endRound();
+            } else {
             this.switchTurn();
-        }
+            }
+        },
+
+        checkWinner: function(player, board) {
+            const symbol: player.symbol;
+
+            const winningCombinations = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]
+            ];
+
+            for (const combination of winningCombinations) {
+                if (
+                    board[combination[0]] === symbol &&
+                    board[combination[1]] === symbol &&
+                    board[combination[2]] === symbol
+                ) {
+                    return true;
+                }
+            }
+
+            return false;
+        },
+
+        setScore: function (checkWinner) {
+            if (checkWinner === player1) {
+                player1.score++;
+                console.log("Player 1 wins this round!");
+            } else if (checkWinner === player2) {
+                player2.score++;
+                console.log("Player 2 wins this round!");
+            } else {
+                console.log("It's a draw!");
+            }
+        },
+
+        endRound: function () {
+            roundsPlayed++;
+            if (roundsPlayed < 5) {
+                console.log(`Round ${roundsPlayed} is over. Resetting the board.`);
+                gameboard.resetBoard();
+            } else {
+                console.log(`Game Over! 5 rounds have been played!`);
+                this.declareOverallWinner();
+            }
+        },
+
+        declareOverallWinner: function() {
+            if (player1.score > player2.score) {
+                console.log(`${player1.name} is the overall winner!`);
+            } else if (player2.score > player1.score) {
+                console.log(`${player2.name} is the overall winner!`);
+            } else {
+                console.log("The game ends in a draw!");
+            }
+        },
     };
 };
 
-const gameController = gameControllerFactory(player1, player2, gameboard);
-gameController.displayCurrentPlayer();
-gameController.playMove(0);
-gameController.displayCurrentPlayer();
+// console log to check above code.
+    const gameController = gameControllerFactory(player1, player2, gameboard);
+    gameController.displayCurrentPlayer();
+    gameController.playMove(0);
+    gameController.displayCurrentPlayer();
+    gameController.playMove(0);
+    gameController.displayCurrentPlayer();
