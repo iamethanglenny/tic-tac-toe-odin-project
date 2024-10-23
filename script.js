41c56e87773ab96bf6273379f7aa59462d3d2cd5
iamethanglenny <ethanglenny@gmail.com>
2024-10-23 09:59:39 +0200

    feat: add three factory functions, gameboard, player, and gameboardcontroller -- all functions working correctly in console log.

--- script.js
+++ script.js
@@ -2,29 +2,50 @@ const gameboardFactory = () => {
     return {
         board: ["", "", "", "", "", "", "", "", ""], // 3x3 grid for tic-tac-toe
         displayBoard: function () {
-            return this.board;
+            console.log(this.board);
         },
         setMove: function (index, symbol) {
-            if (this.board === "") {
+            if (this.board[index] === "") {
                 this.board[index] = symbol;
             }
-        },
-        resetBoard: function () {
-            return this.board;
         }
     };
 };
 
-const gameControllerFactory = () => {
-
-}
+const gameboard = gameboardFactory();
+gameboard.setMove(0, "X");
+gameboard.displayBoard();
 
 const playerFactory = (name, symbol) => {
     return {
         name: name,
-        symbol: symbol
+        symbol: symbol,
     };
 };
 
 const player1 = playerFactory("Player 1", "X");
 const player2 = playerFactory("Player2", "O");
+
+console.log(player1);
+
+const gameControllerFactory = (player1, player2, gameboard) => {
+    let currentPlayer = player1;
+
+    return {
+        switchTurn: function () {
+            currentPlayer = currentPlayer === player1 ? player2 : player1;
+        },
+        displayCurrentPlayer: function () {
+            console.log(`It's ${currentPlayer.name}'s turn to move.`);
+        },
+        playMove: function (index) {
+            gameboard.setMove(index, currentPlayer.symbol);
+            this.switchTurn();
+        }
+    };
+};
+
+const gameController = gameControllerFactory(player1, player2, gameboard);
+gameController.displayCurrentPlayer();
+gameController.playMove(0);
+gameController.displayCurrentPlayer();
